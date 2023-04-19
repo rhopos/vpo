c1df3bea50e6f96548c15d03c49c5cc822cd42a2
rhopos <kthopos@gmail.com>
2023-04-19 12:57:08 +0300

    2

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
-  url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  url: process.env.URL || "https://vpo.wiki",
+  siteName: " Гуманітарні центри ВПО",
   siteDescription:
     "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
   authorName: "Stephanie Eckles",
