acb4ba01f58edb31c04cdc6966d53d57ae59cd44
rhopos <kthopos@gmail.com>
2023-04-19 13:22:43 +0300

    _

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: process.env.URL || "https://vpo.wiki",
   siteName: " Гуманітарні центри ВПО",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Перелік гуманітарних центрів, громадських організацій, райвиконкомів та інших місць де можна отримати соціальну та гуманітарну допомогу для ВПО в Кривому Розі",
+  authorName: "Rhopos",
+  twitterUsername: "noname_pain", // no `@`
 };
